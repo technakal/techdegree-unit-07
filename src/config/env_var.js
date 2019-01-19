a0896def8e571fea7335ff5f4a8a27b3c93d402d
Noel Keener <noel@nakeener.com>
2019-01-19 07:18:15 -0600

    Try env var changes

--- src/config/env_var.js
+++ src/config/env_var.js
@@ -1,8 +1,10 @@
 /**
  * Returns the appropriate credentials, depending on whether the application is deployed or in development.
  */
-if (process.env.NODE_ENV === 'production') {
+if (process.env.production) {
+  // use prod keys
   module.exports = require('./prod');
 } else {
+  // use dev keys
   module.exports = require('./config');
 }
