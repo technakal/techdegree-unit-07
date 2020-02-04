f6e9f15c44b21140d9d703e5c5b1ad4d98bb5ac6
Noel Keener <noel@nakeener.com>
2020-02-04 15:05:05 -0600

    Renamed some variables
    I had some environment variables named incorrectly. Needed to add the REACT_APP_ prefix.

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  FLICKR_KEY: process.env.FLICKR_KEY,
-  FLICKR_SECRET: process.env.FLICKR_SECRET,
+  REACT_APP_FLICKR_KEY: process.env.FLICKR_KEY,
+  REACT_APP_FLICKR_SECRET: process.env.FLICKR_SECRET,
 };
